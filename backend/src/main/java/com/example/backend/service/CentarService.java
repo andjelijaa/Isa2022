06502314abe6780018ca349f2cac2f83e2599d09
andjelijaa <andjelija.djordjevic1997@gmail.com>
2022-12-07 22:35:38 +0100

    Dodavanje sorta za centre

--- backend/src/main/java/com/example/backend/service/CentarService.java
+++ backend/src/main/java/com/example/backend/service/CentarService.java
@@ -1,12 +1,13 @@
 package com.example.backend.service;
 
+import com.example.backend.models.request.SortCentarDto;
 import com.example.backend.models.response.CentarDto;
 
 import java.util.List;
 
 public interface CentarService {
 
-    List<CentarDto> getAllCentri();
+    List<CentarDto> getAllCentri(SortCentarDto sortCentarDto);
 
     CentarDto getCentarById(Long centarId) throws Exception;
 
